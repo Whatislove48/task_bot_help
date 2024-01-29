b664f07122690a16b1daa330036cb767303c9284
WladiSlave <cool-smit3@yandex.ru>
2024-01-29 09:41:15 +0300

    Task BotHelp
    - autoload

--- app/index.php
+++ app/index.php
@@ -1,9 +1,28 @@
+
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="/app/main.css">
+    <title>Задачка BotHelp</title>
+</head>
+<body class="simple">
+
+<h1>
+    Task
+</h1>
+<hr>
+
 <?php
 
 require_once "vendor/autoload.php";
 
 require_once "private/index.php" ;
 
-echo "END";
+echo " <hr>-- END App --";
 
 ?>
+
+</body>
+</html>
+
--- app/private/index.php
+++ app/private/index.php
@@ -1,21 +1,13 @@
-<html lang="ru">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Задачка BotHelp</title>
-</head>
-<body>
-
-<h1>
-    Task
-</h1>
-<hr>
-
 <?php
 
-echo "-- App --";
+
+use Rabbit\Car;
+
+$car = new Car();
+
+
+$car->go();
+
+
 
 ?>
-
-</body>
-</html>
